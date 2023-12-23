f3f61453b3381f17d99b767ddea510d68c9de07e
Lang <74222989+Lang-1210@users.noreply.github.com>
2023-12-24 00:58:58 +0800

    Update index.js
    Update the index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wx5ca5b0129604a7bb",
+  APP_ID: "wx7b531ab1a9f1a0d5",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "ce39111ff1f10cde270b848746419e9e",
+  APP_SECRET: "7cddc33cd541d70c8a4bcf3b7f9e37d9",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -62,9 +62,9 @@ export const config = {
       // 想要发送的人的名字
       name: "me1",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oNhJM62xRU6yDhKzscIXptqfl39M",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "E-p6TGORJ2Qr0FB-MNAXaJum3BZLagDKb7jwryw--H4",
+      useTemplateId: "NjLpDr-5dTm9vO0NMjRKSh8ohS3Akos0FAXql2ik2ck",
       // 所在省份
       province: "甘肃",
       // 所在城市
