31e293ac01c57baac84c0d17aab2267c88e8379f
Lang <74222989+Lang-1210@users.noreply.github.com>
2024-09-18 23:11:21 +0800

    Update index.js
    Update index.js

--- config/index.js
+++ config/index.js
@@ -66,9 +66,9 @@ export const config = {
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "BkytJaldqZ5SwGwn1p-P1b5IWy2J70CBUOzUdf82j94",
       // 所在省份
-      province: "甘肃",
+      province: "天津",
       // 所在城市
-      city: "兰州",
+      city: "河西",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '12-10',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
